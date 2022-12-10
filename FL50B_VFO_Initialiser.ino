42ddd917a38163b32701bb59d68b69cf3087fe20
G4ALG <steve@alg.myzen.co.uk>
2022-12-10 16:44:02 +0000

    Change of variable names in band array

--- FL50B_VFO_Initialiser.ino
+++ FL50B_VFO_Initialiser.ino
@@ -1,92 +1,92 @@
 /*
-Arduino Nano script for FL-50B transmitter.
-EEPROM initialiser script
+Arduino Nano EEPROM initialiser script for FL-50B transmitter.
+
 */
 
 #include <Wire.h>
 #include <EEPROM.h>
 
-const int NUMBER_OF_BANDS = 5; // number of selectable bands 
+const int NUMBER_OF_BANDS = 5; // number of selectable bands
 
-// struct for 'band parameter set' records -- the parameters that will change with each band
-typedef struct {
-  boolean  active;
-  uint32_t band;
-  uint32_t radix;
-} BandSet_type;
+// Combined typedef and struct for band parameter records: the parameters associated with each band
+typedef struct
+{
+    boolean active;
+    uint32_t Hz;
+    uint32_t radix;
+} BandParameters;
 
-BandSet_type BandSet[NUMBER_OF_BANDS];         // array of band parameter sets
-byte BandIndex;                          // index into BandSet array (representing the current band)
+BandParameters Band[NUMBER_OF_BANDS]; // array of band parameters per band
+byte BandIndex;                       // index into Band array (representing the current band)
 
-int t=0, second_counter=0;
+int t = 0, second_counter = 0;
 
+void setup()
+{
+    Serial.begin(9600);
+    Serial.println("setup()");
 
-void setup(){
-  Serial.begin(9600);  
-  Serial.println("setup()");
+    // print EEPROM size
+    int ee_len = EEPROM.length();
+    Serial.print("EEPROM length =");
+    Serial.println(ee_len);
 
-  // print EEPROM size
-  int ee_len = EEPROM.length();
-  Serial.print("EEPROM length =");
-  Serial.println(ee_len);
-  
-  Serial.print("BandSet[] length =");
-  Serial.println(sizeof(BandSet_type) * NUMBER_OF_BANDS);
+    Serial.print("Band[] length =");
+    Serial.println(sizeof(BandParameters) * NUMBER_OF_BANDS);
 
-  // clear the EEPROM contents
-  for(int j=0; j<ee_len; j++)
-  {
-    EEPROM.write(j, 0);
-  }
+    // clear the EEPROM contents
+    for (int j = 0; j < ee_len; j++)
+    {
+        EEPROM.write(j, 0);
+    }
 
- // initialise BandSet array
-  for(int n=0; n<NUMBER_OF_BANDS; n++) BandSet[n].active = 0;   // make sure all are inactive to start with 
-  BandSet[0]  = (BandSet_type){1,   3560000ULL,  100};
-  BandSet[1]  = (BandSet_type){1,   7030000ULL,  100};
-  BandSet[2]  = (BandSet_type){1,  14060000ULL,  100};
-  BandSet[3]  = (BandSet_type){1,  21060000ULL,  100};
-  BandSet[4]  = (BandSet_type){1,  28060000ULL,  100};
-//  BandSet[5]  = (BandSet_type){1,   7030000ULL,  100};
-//  BandSet[6]  = (BandSet_type){1,  10116000ULL,  100};
-//  BandSet[7]  = (BandSet_type){1,  14060000ULL,  100};
-//  BandSet[8]  = (BandSet_type){1,  18096000ULL,  100};
-//  BandSet[9]  = (BandSet_type){1,  21060000ULL,  100};
-//  BandSet[10] = (BandSet_type){1,  24906000ULL,  100};
-//  BandSet[11] = (BandSet_type){1,  28060000ULL,  100};
-//  BandSet[12] = (BandSet_type){1,  50060000ULL,  100};
-//  BandSet[13] = (BandSet_type){1,  70200000ULL,  100};
-//  BandSet[14] = (BandSet_type){1, 144060000ULL,  100};
-  
+    // initialise Band array
+    for (int n = 0; n < NUMBER_OF_BANDS; n = n + 1)
+        Band[n].active = 0; // make sure all are inactive to start with
+    Band[0] = (BandParameters){1, 3560000ULL, 100};
+    Band[1] = (BandParameters){1, 7030000ULL, 100};
+    Band[2] = (BandParameters){1, 14060000ULL, 100};
+    Band[3] = (BandParameters){1, 21060000ULL, 100};
+    Band[4] = (BandParameters){1, 28060000ULL, 100};
+    //  Band[5]  = (BandParameters){1,   7030000ULL,  100};
+    //  Band[6]  = (BandParameters){1,  10116000ULL,  100};
+    //  Band[7]  = (BandParameters){1,  14060000ULL,  100};
+    //  Band[8]  = (BandParameters){1,  18096000ULL,  100};
+    //  Band[9]  = (BandParameters){1,  21060000ULL,  100};
+    //  Band[10] = (BandParameters){1,  24906000ULL,  100};
+    //  Band[11] = (BandParameters){1,  28060000ULL,  100};
+    //  Band[12] = (BandParameters){1,  50060000ULL,  100};
+    //  Band[13] = (BandParameters){1,  70200000ULL,  100};
+    //  Band[14] = (BandParameters){1, 144060000ULL,  100};
 
-  // write the current band index (into BandSet[]) into EEPROM byte 0
-  byte b = 0;
-  EEPROM.write(0, b);
+    // write the current band index (into Band[]) into EEPROM byte 0
+    byte b = 0;
+    EEPROM.write(0, b);
 
-  // put the entire BandSet array into EEPROM
-  int element_len = sizeof(BandSet_type);
-  for(int i=0; i<NUMBER_OF_BANDS; i++)
-  {
-    EEPROM.put(1 + (i * element_len), BandSet[i]);
-  };
+    // put the entire Band array into EEPROM
+    int element_len = sizeof(BandParameters);
+    for (int i = 0; i < NUMBER_OF_BANDS; i = i + 1)
+    {
+        EEPROM.put(1 + (i * element_len), Band[i]);
+    };
 
-// dump out the BandSet array for diagnostics
-  for(int n=0; n < NUMBER_OF_BANDS ; n++)
-  {
-    Serial.print((int)BandSet[n].active);
-    Serial.print(" ");
-    Serial.print(BandSet[n].band);
-    Serial.print(" ");
-    Serial.print((long)BandSet[n].radix);
-    Serial.println();
-  }
+    // dump out the Band array for diagnostics
+    for (int n = 0; n < NUMBER_OF_BANDS; n = n + 1)
+    {
+        Serial.print((int)Band[n].active);
+        Serial.print(" ");
+        Serial.print(Band[n].Hz);
+        Serial.print(" ");
+        Serial.print((long)Band[n].radix);
+        Serial.println();
+    }
 
     t = millis();
 }
 
-
 void loop()
 {
-  /*
+    /*
   if((millis() - t) > 1000){
     t=millis();
     second_counter++;
